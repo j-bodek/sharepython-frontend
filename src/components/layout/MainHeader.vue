df2f49fce56d5016d8c42d078b2a2f08fde5e8c4
Liljack118 <kamilzajaczek4@gmail.com>
2022-12-14 20:31:46 +0100

    modify header with mdbootstrap components

--- src/components/layout/MainHeader.vue
+++ src/components/layout/MainHeader.vue
@@ -1,57 +1,89 @@
 <template>
     <header>
-        <!-- Navbar -->
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark w-100 position-fixed">
-            <!-- Container wrapper -->
-            <div class="container">
-                <!-- Navbar brand -->
-                <router-link class="navbar-brand me-2 fw-bold" to="/">
-                    SharePython
-                </router-link>
+        <MDBNavbar expand="md" class="navbar navbar-expand-md navbar-dark bg-dark w-100 position-fixed" container>
+            <!-- Navbar brand -->
+            <router-link class="navbar-brand me-2 fw-bold" to="/">
+                SharePython
+            </router-link>
+            <MDBNavbarToggler @click="collapse1 = !collapse1" target="#navbarSupportedContent"></MDBNavbarToggler>
+            <MDBCollapse class="justify-content-end" v-model="collapse1" id="navbarSupportedContent">
 
-                <!-- Toggle button -->
-                <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
-                    data-mdb-target="#navbarButtonsExample" aria-controls="navbarButtonsExample" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <i class="fas fa-bars"></i>
-                </button>
-
-                <!-- Collapsible wrapper -->
-                <div class="collapse navbar-collapse justify-content-end" id="navbarButtonsExample">
-                    <div class="d-flex align-items-center">
-                        <div class="d-flex" v-if="(user === null)">
-                            <router-link to="/login" class="nav-link btn shadow-0 text-light px-3 me-2">
-                                Log In
-                            </router-link>
-                            <router-link to="/signup" class="nav-link btn shadow-0 text-light me-3">
-                                Sign Up
-                            </router-link>
-                        </div>
-                        <div class="d-flex" v-else>
-                            <button v-on:click="logoutUser" class="nav-link btn shadow-0 text-light me-3">
-                                LogOut
-                            </button>
-                            <router-link to="/settings" class="nav-link btn shadow-0 text-light me-3">
-                                {{ user.first_name }} {{ user.last_name }}
-                            </router-link>
-                        </div>
-                        <a class="btn btn-light px-3" target="_blank" href="https://github.com/LilJack118/sharepython"
-                            role="button">
-                            <i class="fab fa-github"></i>
-                        </a>
-                    </div>
+                <div class="d-flex align-items-center w-md-100">
+                    <MDBNavbarNav v-if="(user === null)" class="w-100">
+                        <MDBNavbarItem class="nav-link nav-link-bg m-0 me-2 py-0 px-2 rounded-4" to="/login" active>
+                            Log In
+                        </MDBNavbarItem>
+                        <MDBNavbarItem class="nav-link nav-link-bg m-0 py-0 px-2 rounded-4" to="/signup" active>
+                            Sign Up
+                        </MDBNavbarItem>
+                    </MDBNavbarNav>
+                    <MDBNavbarNav v-else class="w-100">
+                        <button v-on:click="logoutUser" class="nav-link nav-link-bg btn shadow-0 text-light me-3 px-3">
+                            LogOut
+                        </button>
+                        <MDBNavbarItem>
+                            <!-- Navbar dropdown -->
+                            <MDBDropdown class="nav-item" v-model="dropdown1">
+                                <MDBDropdownToggle tag="a" class="nav-link text-white" @click="dropdown1 = !dropdown1">
+                                    {{ user.first_name }} {{ user.last_name }}
+                                </MDBDropdownToggle>
+                                <MDBDropdownMenu aria-labelledby="dropdownMenuButton">
+                                    <MDBDropdownItem class="dropdown-item" to="#">Your CodeSpaces</MDBDropdownItem>
+                                    <MDBDropdownItem class="dropdown-item" to="#">New CodeSpace</MDBDropdownItem>
+                                    <MDBDropdownItem class="dropdown-item" to="/settings">Account Settings
+                                    </MDBDropdownItem>
+                                </MDBDropdownMenu>
+                            </MDBDropdown>
+                        </MDBNavbarItem>
+                    </MDBNavbarNav>
                 </div>
-                <!-- Collapsible wrapper -->
-            </div>
-            <!-- Container wrapper -->
-        </nav>
+
+            </MDBCollapse>
+
+        </MDBNavbar>
+        <!-- Navbar -->
+
         <!-- Navbar -->
     </header>
 </template>
 
 <script>
+import {
+    MDBBtn,
+    MDBNavbar,
+    MDBNavbarToggler,
+    MDBNavbarBrand,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBCollapse,
+    MDBDropdown,
+    MDBDropdownToggle,
+    MDBDropdownMenu,
+    MDBDropdownItem
+} from 'mdb-vue-ui-kit';
+import { ref } from 'vue';
+
 export default {
     name: "MainHeader",
+    components: {
+        MDBBtn,
+        MDBNavbar,
+        MDBNavbarToggler,
+        MDBNavbarBrand,
+        MDBNavbarNav,
+        MDBNavbarItem,
+        MDBCollapse,
+        MDBDropdown,
+        MDBDropdownToggle,
+        MDBDropdownMenu,
+        MDBDropdownItem
+    },
+    data() {
+        return {
+            collapse1: ref(false),
+            dropdown1: ref(false),
+        }
+    },
     computed: {
         user() {
             return this.$store.getters["getUser"];
@@ -70,8 +102,16 @@ export default {
 </script>
 
 <style>
-.nav-link:hover {
+.dropdown-item:hover {
+    cursor: pointer;
+}
+
+.nav-link-bg:hover {
     color: #332d2d !important;
-    background-color: #fbfbfb;
+    background-color: #fbfbfb !important;
+}
+
+.nav-link-bg:hover a {
+    color: #332d2d !important;
 }
 </style>
