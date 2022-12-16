ec45168635f19d6cb1de873b8d4553d3a9761312
Liljack118 <kamilzajaczek4@gmail.com>
2022-12-16 19:57:45 +0100

    add github main repository link in header

--- src/components/layout/MainHeader.vue
+++ src/components/layout/MainHeader.vue
@@ -9,7 +9,7 @@
             <MDBCollapse class="justify-content-end" v-model="collapse1" id="navbarSupportedContent">
 
                 <div class="d-flex align-items-center w-md-100">
-                    <MDBNavbarNav v-if="(user === null)" class="w-100">
+                    <MDBNavbarNav v-if="(user === null)" class="w-100 p-0">
                         <MDBNavbarItem class="nav-link nav-link-bg m-0 me-2 py-0 px-2 rounded-4" to="/login" active>
                             Log In
                         </MDBNavbarItem>
@@ -17,8 +17,9 @@
                             Sign Up
                         </MDBNavbarItem>
                     </MDBNavbarNav>
-                    <MDBNavbarNav v-else class="w-100">
-                        <button v-on:click="logoutUser" class="nav-link nav-link-bg btn shadow-0 text-light me-3 px-3">
+                    <MDBNavbarNav v-else class="w-100 p-0">
+                        <button v-on:click="logoutUser"
+                            class="nav-link nav-link-bg btn shadow-0 text-light me-md-3 px-3">
                             LogOut
                         </button>
                         <MDBNavbarItem>
@@ -37,6 +38,10 @@
                         </MDBNavbarItem>
                     </MDBNavbarNav>
                 </div>
+                <a class="btn btn-light px-3 my-0 ms-md-3 m-0 d-block" target="_blank"
+                    href="https://github.com/LilJack118/sharepython" role="button">
+                    <i class="fab fa-github"></i>
+                </a>
 
             </MDBCollapse>
 
