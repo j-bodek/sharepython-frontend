f3db237efaa8537860bb1aadea37a1385529ab40
Liljack118 <kamilzajaczek4@gmail.com>
2022-12-30 20:07:25 +0100

    style main footer to match header

--- src/components/layout/MainFooter.vue
+++ src/components/layout/MainFooter.vue
@@ -1,18 +1,21 @@
 <template>
-    <footer class="bg-light text-center text-lg-start">
+    <footer class="footer bg-dark text-center text-lg-start">
         <!-- Copyright -->
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        <div class="text-center text-light p-3">
             <span>Created by</span>
-            <a class="text-dark fw-bold text-decoration-underline mx-1" href="https://github.com/LilJack118/"
-                target="_blank">
+            <a class="fw-bold text-decoration-underline mx-1" href="https://github.com/LilJack118/" target="_blank">
                 Jakub Bodek
             </a>
             <span>and</span>
-            <a class="text-dark fw-bold text-decoration-underline mx-1" href="https://github.com/skublin/"
-                target="_blank">
+            <a class="fw-bold text-decoration-underline mx-1" href="https://github.com/skublin/" target="_blank">
                 Szymon Kublin
             </a>
         </div>
         <!-- Copyright -->
     </footer>
 </template>
+<style scoped>
+.footer {
+    box-shadow: 0 -4px 12px 0 rgb(0 0 0 / 7%), 0 -2px 4px rgb(0 0 0 / 5%);
+}
+</style>
